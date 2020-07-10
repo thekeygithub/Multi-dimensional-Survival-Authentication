eb0ac07721f666cba2834fbc5a66a9abe2f0a637
lingling fan <458305572@qq.com>
2020-07-10 17:04:31 +0800

    update

--- nodes/HRSS_Bureau/examples/e2e_cli/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash +x
-rm -rf crypto-config
-rm -rf channel-artifacts
-rm -f crypto-config.tar
-rm -f channel-artifacts.tar
-docker stop $(docker ps -aq)
-docker rm $(docker ps -aq)
-docker rmi --force `docker images | grep dev | awk '{print $3}'` 
